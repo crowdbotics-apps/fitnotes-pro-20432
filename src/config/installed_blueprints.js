d64308c8742994a4d1246f206b62762fd29b8d4d
Crowdbotics <team@crowdbotics.com>
2020-09-19 05:31:17 +0000

    add package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'NotificationList5103938', human_name: 'Notification List 5', access_route: 'NotificationList5103938'},
 { name: 'Settings6103937', human_name: 'Settings 6', access_route: 'Settings6103937'},
 { name: 'CalendarView8103935', human_name: 'Calendar View 8', access_route: 'CalendarView8103935', icon: 'calendar'},
 { name: 'TimeTracking12103931', human_name: 'Time Tracking 12', access_route: 'TimeTracking12103931'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import NotificationList5103938Navigator from '../features/NotificationList5103938/navigator';
 import Settings6103937Navigator from '../features/Settings6103937/navigator';
 import CalendarView8103935Navigator from '../features/CalendarView8103935/navigator';
 import TimeTracking12103931Navigator from '../features/TimeTracking12103931/navigator';
@@ -16,6 +17,7 @@ import TimeTracking12103931Navigator from '../features/TimeTracking12103931/navi
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+NotificationList5103938: { screen: NotificationList5103938Navigator },
 Settings6103937: { screen: Settings6103937Navigator },
 CalendarView8103935: { screen: CalendarView8103935Navigator },
 TimeTracking12103931: { screen: TimeTracking12103931Navigator },

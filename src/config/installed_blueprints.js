f9a993fca4aee40ed988b6efd5f356065b621a76
Crowdbotics <team@crowdbotics.com>
2020-09-19 05:30:54 +0000

    add package blueprint-react-native-time-tracking-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'TimeTracking12103931', human_name: 'Time Tracking 12', access_route: 'TimeTracking12103931'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import TimeTracking12103931Navigator from '../features/TimeTracking12103931/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+TimeTracking12103931: { screen: TimeTracking12103931Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
